98eef8a01f757d8c51f6d248bb60fec0de5ba9d5
Lauriano <laurianocarlos@hotmail.com>
2024-03-04 09:44:06 -0300

    Entidade User e resources

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1l, "maria","maria@gmail.com","11-23822883", "1234");
+        return ResponseEntity.ok().body(user);
+    }
+}
